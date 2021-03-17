b411ec81e3a2fd266dbd216d5bc7d44e6fb5b6d4
Dimitar Betsov <dbetsov@cleancodefactory.de>
2021-03-17 18:01:20 +0200

    adding a fake web server

--- index.js
+++ index.js
@@ -72,3 +72,10 @@ client.on('message', message => {
 });
 
 client.login(process.env.TOKEN);
+
+const http = require('http');
+const server = http.createServer((req, res) => {
+	res.writeHead(200);
+	res.end('ok');
+});
+server.listen(3000);
