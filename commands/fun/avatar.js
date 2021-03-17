5b35983cfbf06995b8510fd08235cc52e135de67
Dimitar Betsov <dbetsov@cleancodefactory.de>
2021-03-17 17:32:02 +0200

    Fixed the avatar command.

--- commands/fun/avatar.js
+++ commands/fun/avatar.js
@@ -3,13 +3,15 @@ module.exports = {
 	description: 'Returns the avatar of a specified user.',
 	cooldown: 10,
 	args: true,
-	execute(message, args) {
-		const guildMember = message.guild.members.fetch({ query: args[0], limit: 1 });
+	async execute(message, args) {
+		let guildMember = await message.guild.members.fetch({ query: args[0], limit: 1 });
+		guildMember = guildMember.array()[0];
+		const user = await message.client.users.fetch(guildMember.id);
 
 		if(guildMember) {
 			message.channel.send({
 				files: [{
-					attachment: `${message.author.displayAvatarURL({ format: 'png', dynamic: true })}`,
+					attachment: `${user.displayAvatarURL({ format: 'png', dynamic: true })}`,
 					name: 'file.jpg',
 				}],
 			});
